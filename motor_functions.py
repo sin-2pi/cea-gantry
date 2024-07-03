7be5b7e7e1cf450896f0e7fea7a73f7b29fb9a60
Nile <nyoung8@uwyo.edu>
2024-07-03 16:37:36 -0600

    Multithreading for vertical motors.

--- motor_functions.py
+++ motor_functions.py
@@ -1,22 +1,23 @@
 import time
 import cv2
+import threading
 
 from src.TMC_2209.TMC_2209_StepperDriver import *
 from src.TMC_2209._TMC_2209_GPIO_board import Board
 
 # Constructor, sets GPIO pins used to communicate with the RPi5.
-payload_motor = TMC_2209(21, 16, 20, skip_uart_init=True)
-#vert_motor1 = TMC_2209(17, 27, 22, skip_uart_init=True)
-#vert_motor2 = TMC_2209(23, 24, 25, skip_uart_init=True)
+payloadMotor = TMC_2209(21, 16, 20, skip_uart_init=True)
+vertMotor1 = TMC_2209(17, 27, 22, skip_uart_init=True)
+vertMotor2 = TMC_2209(23, 24, 25, skip_uart_init=True)
 
 # Setting max speed and acceleration in terms of fullsteps per second.
-payload_motor.set_acceleration_fullstep(2500)
-payload_motor.set_max_speed_fullstep(3000)
+payloadMotor.set_acceleration_fullstep(2500)
+payloadMotor.set_max_speed_fullstep(3000)
 
 # This variable is meant to be the end point for camera data collection, on the horizontal axis.
 # Change this
-gantry_end_hzn = 16000
-gantry_end_vrt = 16000
+gantryEndHzn = 16000
+gantryEndVrt = 16000
 
 # This variable is meant to be the distance (in steps) from one vertical tower cell to the next, horizontally.
 # Change this
@@ -31,28 +32,66 @@ Then continues until the set endpoint of the gantry system is reached and the mo
 """
 
 
-def move_motor_horizontally():
+def mainMotorMovement():
 
-    step = 2000
-    payload_motor.set_motor_enabled(True)
-    payload_motor.set_current_position(0)
-    while payload_motor.get_current_position() < gantry_end_hzn:
-        payload_motor.run_to_position_steps(step)
-        step += 2000
+    step = 1626
+    payloadMotor.set_motor_enabled(True)
+    vertMotor1.set_motor_enabled(True)
+    vertMotor2.set_motor_enabled(True)
+
+    payloadMotor.set_current_position(0)
+    vertMotor1.set_current_position(0)
+    vertMotor2.set_current_position(0)
+
+    while vertMotor1.get_current_position() < gantryEndVrt:
+        moveMotorVertically()
+        step += 1626
         time.sleep(1)
-        camera_snapshot()
+        cameraSnapshot()
         time.sleep(1)
-    payload_motor.run_to_position_steps(0)
-    #if payload_motor.get_current_position() == 0:
-        #move_motor_horizontally()
+    moveVrtHome()
+    if payloadMotor.get_current_position() == 0:
+        moveMotorHorizontally()
 
 
-def move_motor_vertically():
-    vert_motor1.run_to_position_steps(1626)
-    vert_motor2.run_to_position_steps(1626)
+def moveMotorHorizontally():
+    payloadMotor.run_to_position_steps()
+
+def moveMotorVertically():
+    def verticalMovement1(step):
+        vertMotor1.run_to_position_steps(step)
+
+    def verticalMovement2(step):
+        vertMotor2.run_to_position_steps(step)
+
+    thread1 = threading.Thread(target=verticalMovement1)
+    thread2 = threading.Thread(target=verticalMovement2)
+
+    thread1.start()
+    thread2.start()
 
 
-def camera_snapshot():
+    thread1.join()
+    thread2.join()
+
+def moveVrtHome():
+    def verticalHome1():
+        vertMotor1.run_to_position_steps(0)
+
+    def verticalHome2():
+        vertMotor2.run_to_position_steps(0)
+
+    thread3 = threading.Thread(target=verticalHome1)
+    thread4 = threading.Thread(target=verticalHome2)
+
+    thread3.start()
+    thread4.start()
+
+
+    thread3.join()
+    thread4.join()
+
+def cameraSnapshot():
     cap = cv2.VideoCapture(0)
 
     # Set the resolution
@@ -79,3 +118,4 @@ def camera_snapshot():
 
 def move_motor_home():
     pass
+
