fc3da1c3ccd8607bc2f37e168974d43d07547e44
Nile Young <nyoung8@uwyo.edu>
2025-05-16 15:51:12 -0600

    Update motormovement.py

--- motormovement.py
+++ motormovement.py
@@ -1,102 +1,107 @@
 import threading
 import time
-from typing import Dict
-# from camera_control import CameraControl
 import subprocess
 from src.TMC_2209.TMC_2209_StepperDriver import TMC_2209
 
+# May need to swap the - step to achieve proper movement directions
+stepR_global = -20000
+stepL_global = 20000
 
-class MotorControl:
-    def __init__(self):
-        self.payloadMotor = TMC_2209(5, 6, 26, skip_uart_init=True)
-        self.vertMotor2 = TMC_2209(17, 27, 22, skip_uart_init=True)
-        self.vertMotor1 = TMC_2209(23, 24, 25, skip_uart_init=True)
 
-        self.width = 1920
-        self.height = 1080
-        self.gantryEndHzn = 100000
-        self.gantryEndVrt = 75000#halfway is about 75000
-        self.stepRight = 25000 # this is one
-        self.stepLeft = -25000 # this is two
-        self.stepHzn = 20000
+def setup_motors():
+    global payloadMotor, vertMotor1, vertMotor2
+    
+    payloadMotor = TMC_2209(5, 6, 26, skip_uart_init=True)
+    vertMotor2 = TMC_2209(17, 27, 22, skip_uart_init=True)
+    vertMotor1 = TMC_2209(23, 24, 25, skip_uart_init=True)
+    
+    for motor in [vertMotor1, vertMotor2, payloadMotor]:
+        motor.set_acceleration_fullstep(7500)
+        motor.set_max_speed_fullstep(7500)
+        motor.set_motor_enabled(True)
+        motor.set_current_position(0)
 
-        self._setup_motors()
+def take_snapshot(frame_count=100):
+    width = 1920
+    height = 1080
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+    file_name = f"captured_image_{timestamp}.jpg"        
+    command = [
+        'fswebcam',
+        '-r', f'{width}x{height}',
+        '--jpeg', '85',
+        '-D', '1', '-F', '80',
+        file_name
+    ]
+    
+    try:
+        subprocess.run(command, check=True)
+        print(f"Photo taken and saved as '{file_name}'.")
+    except subprocess.CalledProcessError:
+        print("Failed to capture photo.")
 
-    def _setup_motors(self):
-        for motor in [self.vertMotor1, self.vertMotor2, self.payloadMotor]:
-            motor.set_acceleration_fullstep(2500)
-            motor.set_max_speed_fullstep(7000)
-            motor.set_motor_enabled(True)
-            motor.set_current_position(0)
+def move_motor_vertically():
+    global stepR_global, stepL_global
 
-    def take_snapshot(self, frame_count: int = 100):
-        timestamp = time.strftime("%Y%m%d-%H%M%S")
-        file_name = f"captured_image_{timestamp}.jpg"        
-        command = [
-            'fswebcam',
-            '-r', f'{self.width}x{self.height}',
-            '--jpeg', '85',
-            '-D', '1', '-F', '80',
-            file_name
-        ]
+    threads = [
+        threading.Thread(
+            target=motor.run_to_position_steps, args=(stepL_global, stepR_global)
+        )
+        for motor in [vertMotor1, vertMotor2]
+    ]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+    stepR_global += 25000
+    stepL_global -= 25000
+    
+def testfunc():
+    move_motor_vertically()
+def move_vrt_home():
+    threads = [
+        threading.Thread(target=motor.run_to_position_steps, args=(0,))
+        for motor in [vertMotor1, vertMotor2]
+    ]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+def main_motor_movement():
+    gantryEndHzn = 40000
+    gantryEndVrt = 150000
+    stepHzn = -30000
+    
+    loopcounter = 0
+    while abs(vertMotor1.get_current_position()) < gantryEndVrt:
+        print(f"Vert Motor: {abs(vertMotor2.get_current_position())} < {gantryEndVrt}")
+        print(f"Payload Motor: {payloadMotor.get_current_position()} < {gantryEndHzn}")
+
+        take_snapshot()
+        print(payloadMotor.get_current_position())
+        time.sleep(.5)
+        if loopcounter == 0:
+            payloadMotor.run_to_position_steps(-45000)
+            take_snapshot()
+            loopcounter = 1
         
-        try:
-            subprocess.run(command, check=True)
-            print(f"Photo taken and saved as '{file_name}'.")
-        except subprocess.CalledProcessError:
-            print("Failed to capture photo.")
+        payloadMotor.run_to_position_steps(stepHzn)
+        take_snapshot()
+        stepHzn += 15000
+        
+        if payloadMotor.get_current_position() >= gantryEndHzn:
+            take_snapshot()
+            payloadMotor.run_to_position_steps(0)
+            print(f"Expected vertical movement.Triggered {payloadMotor.get_current_position()} >= {gantryEndHzn}")
+            move_motor_vertically()
 
-    def main_motor_movement(self):
-            while self.vertMotor1.get_current_position() < self.gantryEndVrt:
-                print(f"Looping: {self.vertMotor1.get_current_position()} < {self.gantryEndVrt}")
-                self.take_snapshot()
-                print(self.payloadMotor.get_current_position())
-                time.sleep(.5)
-                self.move_motor_horizontally()
-                self.stepHzn += 20000
-                if self.payloadMotor.get_current_position() >= self.gantryEndHzn:
-                    self.take_snapshot()
-                    self.payloadMotor.run_to_position_steps(0)
-                    self.stepHzn = 0
+            stepHzn = -15000
+            loopcounter = 0
 
-                    self.move_motor_vertically()
-                    self.stepRight += 25000
-                    self.stepLeft -= 25000
-                # if self.payloadMotor.get_current_position() == 0:
-                #     self.move_motor_horizontally()
-            self.move_vrt_home()
-
-
-    def move_motor_horizontally(self):
-        self.payloadMotor.run_to_position_steps(self.stepHzn)
-
-    def move_motor_vertically(self):
-        threads = [
-            threading.Thread(
-                target=motor.run_to_position_steps, args=(self.stepLeft, self.stepRight)
-            )
-            for motor in [self.vertMotor1, self.vertMotor2]
-        ]
-        for thread in threads:
-            thread.start()
-        for thread in threads:
-            thread.join()
-
-    def move_vrt_home(self):
-        threads = [
-            threading.Thread(target=motor.run_to_position_steps, args=(0,))
-            for motor in [self.vertMotor1, self.vertMotor2]
-        ]
-        for thread in threads:
-            thread.start()
-        for thread in threads:
-            thread.join()
-
-    # def move_motor_home(self):
-    #     for motor in [self.payloadMotor]:
-    #         motor.run_to_position_steps(0)
-
+    move_vrt_home()
 
 if __name__ == "__main__":
-    motor_control = MotorControl()
-    motor_control.main_motor_movement()
+    setup_motors()
+    main_motor_movement()
+    #move_motor_vertically()
